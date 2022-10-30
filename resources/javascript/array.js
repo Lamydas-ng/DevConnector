4e890cf3b5cc0367d30e61d97c7724a6ecc8b016
lamydas-ng <lamydas.ng@gmail.com>
2022-10-30 11:51:53 +0100

    Added palindrome code snippet

---
+++ resources/javascript/array.js
@@ -0,0 +1,48 @@
+// array is collection of similar type content.
+// 10,20,30,40,50,60,70,80,90,100
+var a = [10, 2, 351, -42, -53];
+
+//array position reference will start at 0.
+// the last index : size-1
+
+for (const i of a) {
+  console.log(i);
+}
+// let is used to declare the variable
+// var and let and const diff.
+
+for (let i = 0; i < a.length-1; i++) {
+    let element = a[i];
+    if (a[i] % 2 == 0)
+        console.log(a[i]);
+
+  //      console.log(element);
+}
+
+// array is collection of similar type content.
+// 10,20,30,40,50,60,70,80,90,100
+var a = [10, 2, 351, -42, -53];
+
+//array position reference will start at 0.
+// the last index : size-1
+
+for (const i of a) {
+  console.log(i);
+}
+// let is used to declare the variable
+// var and let and const diff.
+
+for (let i = 0; i < a.length; i++) {
+  let element = a[i];
+  console.log(element);
+}
+
+
+
+// print the even numbers from array.
+// you have to declare 2 arrays : 1st array hold the numbers and 2nd array hold the factorials .
+// [1,2,3,4,5] 2nd expected : [1,2,6,24,120]
+
+// declare an array with 10 numbers and find out palindrome numbers in the array.
+// declare an array with 10 numbers and find out prime numbers in the array.
+// declare an array with 10 numbers and find out strong numbers in the array.
--- resources/javascript/factorial.js
+++ resources/javascript/factorial.js
@@ -5,28 +5,53 @@
 // 145
 // if sum of the factorail = original number then that number is called as strong number.
 
-var number = 111;
+// var number = 145;
 
-var str = number.toString();
-var result = 0;
-for (let n = 0; n < str.length; n++) {
-  result = result + factorial(Number(str.charAt(n)));
-}
+// var str = number.toString();
+// var result = 0;
+// for (let n = 0; n < str.length; n++) {
+//   result = result + factorial(Number(str.charAt(n)));
+// }
 
-if(parseInt(result,10) === number)
-    console.log(`Factorial of ${number} is a strong number: ${result}`);
-else
-    console.log(`factorial of ${number} is not a strong number: ${result}`);
+// if(parseInt(result,10) === number)
+//     console.log(`Factorial of ${number} is a strong number: ${result}`);
+// else
+//     console.log(`factorial of ${number} is not a strong number: ${result}`);
 
-function factorial(num) {
-    var result = 1;
+// function factorial(num) {
+//     var result = 1;
 
-    if (num == 0 || num == 1) {
-         return result;
-    }
+//     if (num == 0 || num == 1) {
+//          return result;
+//     }
 
-    for (var i = num ; i >= 1; i--) {
-        result = result * i;
-    }
-    return result;
+//     for (var i = num ; i >= 1; i--) {
+//         result = result * i;
+//     }
+//     return result;
+// }
+
+// Method 2 on strong number factorial
+var number = 145;
+
+var digit = 0;
+var factorial = 1;
+var sum = 0;
+while (number > 0) {
+    // taking mod to get the last digit
+  digit = number % 10;
+  //calculating the factorial fo the digit
+  for (var i = 1; i <= digit; i++) {
+    factorial = factorial * i;
+  }
+  // printing the factorial
+  console.log(factorial);
+  // adding the factorial value to the sum later to check the number is strong or not.
+  sum += factorial; // sum = sum + factorial
+  // reseting the factorial to 1 to get a support to calculate the factorial of the next digit.
+
+  factorial = 1;
+  // dividing the number by 10 to remove the last digit and applying floor function to remove the fraction from it.
+
+  number = Math.floor(number / 10);
 }
