850e64f569bec4ed3f214d268b70f17a1d427903
RUFAI Ahmed <lamydas.ng@gmail.com>
2022-11-04 16:47:32 +0100

    Modified Array of palindrome numbers

--- resources/javascript/palindrome_arr.js
+++ resources/javascript/palindrome_arr.js
@@ -12,7 +12,7 @@
 var number_arr = [323,456,789,101112,125,785,558,721,14,121];
 
 
-for(i=number_arr.length; i>=0; i--){
+for(i=number_arr.length-1; i>=0; i--){
 
    // digit to hold the number.
     var digit = 0;
@@ -20,12 +20,13 @@ for(i=number_arr.length; i>=0; i--){
     var reversal = 0;
     var number = 0;
     number = number_arr[i];
+    var temp = number;
 
     while (number > 0) {
     // do we need to separate the digits and form the number
     
     digit = number % 10;
-
+    
     reversal = reversal * 10 + digit;
     console.log(digit);
     number = Math.floor(number / 10);
@@ -33,7 +34,9 @@ for(i=number_arr.length; i>=0; i--){
         counter++;
     }
 
-    console.log("final number:" + reversal);
-    console.log("no of digits in a number=" + counter);
+    if( reversal == temp)
+    console.log("final number: " + reversal + " is palindrome");
+    else
+    console.log("final number: " + reversal + " is not palindrome");
  }
 
